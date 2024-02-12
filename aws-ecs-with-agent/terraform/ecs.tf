80bb05f08de9cc07551992270d24cc04814ec170
Maidul Islam <maidul98@gmail.com>
2024-02-11 19:25:40 -0500

    remove auto scale

--- aws-ecs-with-agent/terraform/ecs.tf
+++ aws-ecs-with-agent/terraform/ecs.tf
@@ -14,9 +14,9 @@ data "template_file" "cb_app" {
     fargate_cpu        = var.fargate_cpu
     fargate_memory     = var.fargate_memory
     aws_region         = var.aws_region
-    auth_client_id     = "faf7da54-75e1-43aa-84be-06453f0b8fc4"
-    auth_client_secret = "cb8597f1d5bee779a657e863e5f73c8d4761f3e0c3f6d05c9c2820bc96a0a6f1"
-    agent_config       = "aW5maXNpY2FsOgogIGFkZHJlc3M6ICJodHRwczovL2FwcC5pbmZpc2ljYWwuY29tIgogIGV4aXQtYWZ0ZXItYXV0aDogdHJ1ZQphdXRoOgogIHR5cGU6ICJ1bml2ZXJzYWwtYXV0aCIKICBjb25maWc6CiAgICByZW1vdmVfY2xpZW50X3NlY3JldF9vbl9yZWFkOiBmYWxzZQpzaW5rczoKICAtIHR5cGU6ICJmaWxlIgogICAgY29uZmlnOgogICAgICBwYXRoOiAiL2luZmlzaWNhbC1hZ2VudC9hY2Nlc3MtdG9rZW4iCnRlbXBsYXRlczoKICAtIGJhc2U2NC10ZW1wbGF0ZS1jb250ZW50OiBlM3N0SUhkcGRHZ2djMlZqY21WMElDSTJNbVprT1RKaFlUaGlOak01TnpObVpXVXlNMlJsWXpjaUlDSmtaWFlpSUNJdklpQjlmUXA3ZXkwZ2NtRnVaMlVnTGlCOWZRcDdleUF1UzJWNUlIMTlQWHQ3SUM1V1lXeDFaU0I5ZlFwN2V5MGdaVzVrSUgxOUNudDdMU0JsYm1RZ2ZYMD0KICAgIGRlc3RpbmF0aW9uLXBhdGg6IC9pbmZpc2ljYWwtYWdlbnQvLmVudgo="
+    auth_client_id     = "<>"
+    auth_client_secret = "<>"
+    agent_config       = "<>"
   }
 }
 
